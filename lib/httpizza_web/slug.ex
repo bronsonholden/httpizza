533a88d93529af571027dd378ef1f1eaf5838a32
Paul Holden <paul@paulholden.dev>
2023-09-30 23:16:42 -0700

    Add tasty slug generator

---
+++ lib/httpizza_web/slug.ex
@@ -0,0 +1,29 @@
+defmodule HTTPizzaWeb.Slug do
+  @style ~w(
+    deep-dish thin brooklyn sicilian italian buffalo
+    lasagna calzone hot-pocket bites
+  )
+
+  @cheese ~w(
+    mozzarella cheddar provolone gruyere ricotta parmesan
+    gouda
+  )
+
+  @topping ~w(
+    pepperoni ham salami sausage chicken pineapple spinach
+    onion mushroom olive broccolini bacon kale shrimp egg
+    chorizo salami kielbasa jalapeno eggplant zucchini
+    cauliflower corn broccoli steak potato duck cabbage
+    squash sweet-potato relish
+  )
+
+  def generate(max_id \\ 99999) do
+    [
+      Enum.random(@style),
+      Enum.random(@cheese),
+      Enum.random(@topping),
+      "#{:rand.uniform(max_id)}"
+    ]
+    |> Enum.join("-")
+  end
+end
