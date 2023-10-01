dc3a7e0cf0fe35b793df2d804d48eb34acd05eb6
Paul Holden <paul@paulholden.dev>
2023-09-30 21:44:00 -0700

    Update notifier emails

--- lib/httpizza/iam/user_notifier.ex
+++ lib/httpizza/iam/user_notifier.ex
@@ -41,9 +41,6 @@ defmodule HTTPizza.IAM.UserNotifier do
   """
   def deliver_reset_password_instructions(user, url) do
     deliver(user.email, "Reset password instructions", """
-
-    ==============================
-
     Hi #{user.email},
 
     You can reset your password by visiting the URL below:
@@ -51,8 +48,6 @@ defmodule HTTPizza.IAM.UserNotifier do
     #{url}
 
     If you didn't request this change, please ignore this.
-
-    ==============================
     """)
   end
 
@@ -61,9 +56,6 @@ defmodule HTTPizza.IAM.UserNotifier do
   """
   def deliver_update_email_instructions(user, url) do
     deliver(user.email, "Update email instructions", """
-
-    ==============================
-
     Hi #{user.email},
 
     You can change your email by visiting the URL below:
@@ -71,8 +63,6 @@ defmodule HTTPizza.IAM.UserNotifier do
     #{url}
 
     If you didn't request this change, please ignore this.
-
-    ==============================
     """)
   end
 end
