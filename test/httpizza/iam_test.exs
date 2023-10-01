63ab114cd23cf1bfceae79a593a3d3fb84acfc15
Paul Holden <paul@paulholden.dev>
2023-10-01 08:56:31 -0700

    Assert an organization is created for a new registered user

--- test/httpizza/iam_test.exs
+++ test/httpizza/iam_test.exs
@@ -91,6 +91,7 @@ defmodule HTTPizza.IAMTest do
       assert is_binary(user.hashed_password)
       assert is_nil(user.confirmed_at)
       assert is_nil(user.password)
+      assert [_organization] = IAM.list_organizations()
     end
   end
 
