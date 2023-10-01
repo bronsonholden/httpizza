d2c449ccc96e3e5da13b794496599e4f18fb2687
Paul Holden <paul@paulholden.dev>
2023-09-30 21:55:11 -0700

    Fix tests

--- test/httpizza_web/live/user_registration_live_test.exs
+++ test/httpizza_web/live/user_registration_live_test.exs
@@ -50,9 +50,8 @@ defmodule HTTPizzaWeb.UserRegistrationLiveTest do
       # Now do a logged in request and assert on the menu
       conn = get(conn, "/")
       response = html_response(conn, 200)
-      assert response =~ email
       assert response =~ "Settings"
-      assert response =~ "Log out"
+      assert response =~ "Sign out"
     end
 
     test "renders errors for duplicated email", %{conn: conn} do
