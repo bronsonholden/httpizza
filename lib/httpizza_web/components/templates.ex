621aa7db3cb9071a71962c8031c024f572778215
Bronson Holden <bronson@bronsonholden.com>
2023-10-03 08:08:26 -0700

    Add size attribute to container template

--- lib/httpizza_web/components/templates.ex
+++ lib/httpizza_web/components/templates.ex
@@ -1,11 +1,19 @@
 defmodule HTTPizzaWeb.Templates do
   use Phoenix.Component
 
+  attr(:size, :string, default: "lg")
   slot(:inner_block)
 
   def container(assigns) do
     ~H"""
-    <div class="p-2 sm:p-4 lg:p-8 max-w-3xl mx-auto p-4">
+    <div class={[
+      "p-2 sm:p-4 lg:p-8 mx-auto p-4",
+      case @size do
+        "sm" -> "max-w-md"
+        "md" -> "max-w-lg"
+        "lg" -> "max-w-3xl"
+      end
+    ]}>
       <%= render_slot(@inner_block) %>
     </div>
     """
