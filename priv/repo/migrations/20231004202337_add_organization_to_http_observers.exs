a49d947e5fdbded90f5eecebd697d872b77a7bf7
Bronson Holden <bronson@bronsonholden.com>
2023-10-04 14:52:21 -0700

    Add `organization` association to `HTTPObserver`

---
+++ priv/repo/migrations/20231004202337_add_organization_to_http_observers.exs
@@ -0,0 +1,10 @@
+defmodule HTTPizza.Repo.Migrations.AddOrganizationToHttpObservers do
+  use Ecto.Migration
+
+  def change do
+    alter table(:http_observers) do
+      add :organization_id, references(:organizations, type: :binary_id, on_delete: :delete_all),
+        null: false
+    end
+  end
+end
--- test/support/fixtures/observers_fixtures.ex
+++ test/support/fixtures/observers_fixtures.ex
@@ -4,10 +4,14 @@ defmodule HTTPizza.ObserversFixtures do
   entities via the `HTTPizza.Observers` context.
   """
 
+  alias HTTPizza.IAMFixtures
+
   @doc """
   Generate an HTTP Observer.
   """
-  def http_observer_fixture(attrs \\ %{}) do
+  def http_observer_fixture(attrs \\ %{})
+
+  def http_observer_fixture(%{organization: _} = attrs) do
     {:ok, http_observer} =
       attrs
       |> Enum.into(%{
@@ -21,5 +25,9 @@ defmodule HTTPizza.ObserversFixtures do
       |> HTTPizza.Observers.create_http_observer()
 
     http_observer
+  end
+
+  def http_observer_fixture(attrs) do
+    http_observer_fixture(Map.put(attrs, :organization, IAMFixtures.organization_fixture()))
   end
 end
