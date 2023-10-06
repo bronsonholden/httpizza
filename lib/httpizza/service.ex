d9dc5af2c912ee910d6211b0478084f1dd74f5f4
Bronson Holden <bronson@bronsonholden.com>
2023-10-06 10:57:22 -0700

    Head checks now consider comparator...

--- lib/httpizza/service.ex
+++ lib/httpizza/service.ex
@@ -4,12 +4,15 @@ defmodule HTTPizza.Service do
   @spec validate_http_check(%Checks.HTTPHeadCheck{}, Finch.Response.t()) :: %Checks.CheckResult{}
   def validate_http_check(%Checks.HTTPHeadCheck{} = check, %Finch.Response{} = response) do
     kind = to_string(Checks.HTTPHeadCheck)
+
     header = String.downcase(check.header)
+    comparator = check.comparator
     expected = check.value
     received = Enum.find(response.headers, fn {^header, _} -> true end) |> elem(1)
 
-    if header_value_match?(header, expected, received, check.case_sensitive) do
+    if header_value_match?(header, expected, received, comparator, check.case_sensitive) do
       %Checks.CheckResult{
+        kind: kind,
         status: :ok,
         reason: "Received: #{received}"
       }
@@ -17,24 +20,44 @@ defmodule HTTPizza.Service do
       %Checks.CheckResult{
         kind: kind,
         status: :failed,
-        reason: "Expected: #{expected}; received: #{received}"
+        reason: reason(comparator, expected, received)
       }
     end
   end
 
   # in the context of the given header, do the `expected` and `received` string values
   # match when considering case sensitivity?
-  @spec header_value_match?(String.t(), String.t(), String.t(), boolean()) :: boolean()
-  def header_value_match?("location", expected_uri, received_uri, _case_sensitive) do
-    default_path_uri(expected_uri) == default_path_uri(received_uri)
+  def header_value_match?("location", expected_uri, received_uri, comparator, case_sensitive) do
+    header_value_match?(
+      "_location",
+      String.replace_suffix(expected_uri, "/", ""),
+      String.replace_suffix(received_uri, "/", ""),
+      comparator,
+      case_sensitive
+    )
   end
 
-  def header_value_match?(header, expected, received, false) do
-    header_value_match?(header, String.downcase(expected), String.downcase(received), true)
+  def header_value_match?(header, expected, received, comparator, false) do
+    header_value_match?(
+      header,
+      String.downcase(expected),
+      String.downcase(received),
+      comparator,
+      true
+    )
   end
 
-  def header_value_match?(_header, expected, received, true) do
-    expected == received
+  def header_value_match?(_, _, nil, _, _), do: false
+
+  def header_value_match?(_header, expected, received, comparator, true) do
+    case comparator do
+      :contains -> String.contains?(received, expected)
+      :equal_to -> expected == received
+      :starts_with -> String.starts_with?(received, expected)
+      :ends_with -> String.ends_with?(received, expected)
+      :does_not_contain -> not String.contains?(received, expected)
+      :not_equal_to -> expected != received
+    end
   end
 
   @spec default_path_uri(String.t()) :: URI.t()
@@ -46,5 +69,19 @@ defmodule HTTPizza.Service do
     else
       result
     end
+  end
+
+  defp reason(comparator, expected, received) do
+    expr =
+      case comparator do
+        :contains -> "to contain"
+        :equal_to -> "to equal"
+        :starts_with -> "to start with"
+        :ends_with -> "to end with"
+        :does_not_contain -> "to not contain"
+        :not_equal_to -> "to not equal"
+      end
+
+    "Expected #{received} #{expr} #{expected}"
   end
 end
