b21a33d4b60938265969f04eb68102837b2334c6
Bronson Holden <bronson@bronsonholden.com>
2023-10-09 20:56:19 -0700

    Handle request errors

--- lib/httpizza/workers/http_observer_worker.ex
+++ lib/httpizza/workers/http_observer_worker.ex
@@ -6,18 +6,58 @@ defmodule HTTPizza.HTTPObserverWorker do
 
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"id" => id}}) do
-    id
-    |> Observers.get_http_observer!()
-    |> observe()
+    {:ok, _} =
+      id
+      |> Observers.get_http_observer!()
+      |> observe()
+
+    :ok
   end
 
   def observe(%HTTPObserver{} = observer) do
     uri = build_uri(observer)
 
-    {:ok, response} =
+    result =
       Finch.build(observer.method, uri)
       |> Finch.request(HTTPizza.Finch)
 
+    with {:ok, response} <- result do
+      process_response(observer, response)
+    else
+      {:error, _} ->
+        reason = "Unable to complete the HTTP request as configured"
+
+        {:ok, observation} =
+          observation_changeset(observer, %{
+            status: :failed,
+            reason: reason
+          })
+          |> HTTPizza.Repo.insert()
+
+        observation
+    end
+  end
+
+  # Process the observer and return an `Ecto.Changeset` for inserting the
+  # resulting `HTTPObservation`
+  @spec observation_changeset(%HTTPObserver{}, map()) :: Ecto.Changeset.t()
+  defp observation_changeset(%HTTPObserver{} = observer, attrs) do
+    %HTTPObservation{http_observer_id: observer.id}
+    |> Observers.change_http_observation(attrs)
+  end
+
+  @spec build_uri(%HTTPObserver{}) :: String.t()
+  defp build_uri(%HTTPObserver{} = observer) do
+    URI.new!(%URI{
+      host: observer.hostname,
+      port: observer.port,
+      path: observer.path,
+      scheme: if(observer.https, do: "https", else: "http")
+    })
+    |> URI.to_string()
+  end
+
+  defp process_response(%HTTPObserver{} = observer, %Finch.Response{} = response) do
     check_results =
       Enum.map(observer.header_checks, &HTTPizza.Service.validate_header_check(&1, response))
 
@@ -42,24 +82,5 @@ defmodule HTTPizza.HTTPObserverWorker do
       |> HTTPizza.Repo.transaction()
 
     observation
-  end
-
-  # Process the observer and return an `Ecto.Changeset` for inserting the
-  # resulting `HTTPObservation`
-  @spec observation_changeset(%HTTPObserver{}, map()) :: Ecto.Changeset.t()
-  defp observation_changeset(%HTTPObserver{} = observer, attrs) do
-    %HTTPObservation{http_observer_id: observer.id}
-    |> Observers.change_http_observation(attrs)
-  end
-
-  @spec build_uri(%HTTPObserver{}) :: String.t()
-  defp build_uri(%HTTPObserver{} = observer) do
-    URI.new!(%URI{
-      host: observer.hostname,
-      port: observer.port,
-      path: observer.path,
-      scheme: if(observer.https, do: "https", else: "http")
-    })
-    |> URI.to_string()
   end
 end
