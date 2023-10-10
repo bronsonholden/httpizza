a9907c3c297b39e9a5202d5cbbfd09068fb00563
Bronson Holden <bronson@bronsonholden.com>
2023-10-09 19:58:37 -0700

    Group function clause with others

--- lib/httpizza/service.ex
+++ lib/httpizza/service.ex
@@ -34,10 +34,10 @@ defmodule HTTPizza.Service do
     end
   end
 
-  def header_value_match?(_, _, nil, _, _), do: false
-
   # in the context of the given header, do the `expected` and `received` string values
   # match when considering case sensitivity?
+  def header_value_match?(_, _, nil, _, _), do: false
+
   def header_value_match?("location", expected_uri, received_uri, comparator, case_sensitive) do
     header_value_match?(
       "_location",
