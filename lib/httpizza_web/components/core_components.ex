4ba5d5f1f5388bdb877c95be09122c33dda29a61
Bronson Holden <bronson@bronsonholden.com>
2023-10-15 21:26:17 -0700

    Add org deletion

--- lib/httpizza_web/components/core_components.ex
+++ lib/httpizza_web/components/core_components.ex
@@ -39,6 +39,7 @@ defmodule HTTPizzaWeb.CoreComponents do
   attr :id, :string, required: true
   attr :show, :boolean, default: false
   attr :on_cancel, JS, default: %JS{}
+  attr :size, :string, default: "sm"
   slot :inner_block, required: true
 
   def modal(assigns) do
@@ -60,13 +61,20 @@ defmodule HTTPizzaWeb.CoreComponents do
         tabindex="0"
       >
         <div class="flex min-h-full items-center justify-center">
-          <div class="w-full max-w-3xl p-4 sm:p-6 lg:py-8">
+          <div class={[
+            "w-full p-4 sm:p-6 lg:py-8",
+            case @size do
+              "sm" -> "max-w-md"
+              "md" -> "max-w-xl"
+              "lg" -> "max-w-3xl"
+            end
+          ]}>
             <.focus_wrap
               id={"#{@id}-container"}
               phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
               phx-key="escape"
               phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
-              class="shadow-zinc-700/10 ring-zinc-700/10 relative hidden rounded-2xl bg-white p-14 shadow-lg ring-1 transition"
+              class="shadow-zinc-700/10 ring-zinc-700/10 relative hidden rounded-2xl bg-white p-8 shadow-lg ring-1 transition"
             >
               <div class="absolute top-6 right-5">
                 <button
