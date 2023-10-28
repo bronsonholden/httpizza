5df73998b7ff4c0cae2df18b0da3f60707e099c5
Bronson Holden <bronson@bronsonholden.com>
2023-10-28 09:14:01 -0700

    Add check results to emails

--- lib/httpizza_web/views/http_observation_email_view.ex
+++ lib/httpizza_web/views/http_observation_email_view.ex
@@ -3,9 +3,12 @@ defmodule HTTPizzaWeb.HTTPObservationEmailView do
   use Phoenix.Component
   use HTTPizzaWeb, :verified_routes
 
-  def links(assigns) do
+  alias HTTPizza.Observers.HTTPObserver
+
+  def results(assigns) do
     assigns =
       assigns
+      |> assign(:http_observer, assigns.http_observation.http_observer)
       |> assign(
         :observation_url,
         url(
@@ -20,10 +23,31 @@ defmodule HTTPizzaWeb.HTTPObservationEmailView do
       )
 
     ~H"""
+    <h2>
+      Observer:
+      <a href={@observer_url}>
+        <%= @http_observer.method |> to_string() |> String.upcase() %> <%= build_uri(@http_observer) %>
+      </a>
+    </h2>
+
+    <p :for={check <- @http_observation.check_results}>
+      <%= check.status %>: <%= check.reason %>
+    </p>
+
     <p>
-      Click <a href={@observation_url}>here</a> to view the observation,
-      or <a href={@observer_url}>here</a> to view the observer configuration.
+      Click <a href={@observation_url}>here</a> to view the observation in HTTPizza
     </p>
     """
+  end
+
+  # TODO: move to shared util module
+  defp build_uri(%HTTPObserver{} = observer) do
+    URI.new!(%URI{
+      host: observer.hostname,
+      port: observer.port,
+      path: observer.path,
+      scheme: if(observer.https, do: "https", else: "http")
+    })
+    |> URI.to_string()
   end
 end
